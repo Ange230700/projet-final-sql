04c57960e54108743bb62a1a53647ce8cf57bc87
Flavio <flavio.cormerais@ynov.com>
2023-01-26 14:08:59 +0100

    ajout des bases de données sur django

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
-class Projet(models.Model) :
-    titre = models.CharField(max_length=50)
-    description=models.TextField()
-    image_url=models.CharField(max_length=2000)
+class Employe(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    poste_id = models.IntegerField()
+    age = models.IntegerField()
+    departement_id = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    salaire = models.IntegerField()
+    type_contract = models.IntegerField()
+    adresse_email = models.CharField(max_length=100)
+    date_embauche = models.DateField()
+    numero_tel = models.CharField(max_length=100)
+    adresse = models.CharField(max_length=100)
+    date_de_naissance = models.DateField()
+    numero_d_employe = models.CharField(max_length=100)
+    nombre_d_enfants = models.IntegerField()
+    conges_payes_restants = models.IntegerField()
+    responsable_hierarchique = models.IntegerField()
